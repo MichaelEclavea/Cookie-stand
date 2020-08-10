b0240f64b5e4aeae95f1d87a23f9bbc633b8c0b1
MichaelEclavea <usermike671@gmail.com>
2020-08-10 13:27:56 -0700

    html & js template

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict'
+alert("hello");
+var seattle = {
+
+    minPeople: 23,
+    maxPeople: 65,
+    avgCookie: 6.3,
+    render: function() {
+        var seattleCustomers = math.random(minPeople, maxPeople)
+        var cookiesBaked = seattleCustomers * this.avgCookie
+        var parent = document.getElementById('seattle');
+        var listItem = document.createElement('li');
+        listItem.textContent = ``;
+    }
+}
+seattle.render();
+
+var tokyo = {
+    minPeople: 3,
+    maxPeople: 24,
+    avgCookie: 1.2
+}
+
+var dubai = {
+    minPeople: 11,
+    maxPeople: 38,
+    avgCookie: 3.7
+}
+
+var paris = {
+    minPeople: 20,
+    maxPeople: 38,
+    avgCookie: 2.3
+}
+
+var lima = {
+    minPeople: 2,
+    maxPeople: 16,
+    avgCookie: 4.6
+}
+1: 13 PM | Today
